13aeed734f98714d78e1d692fa971f389eb33ce2
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-07 13:27:56 +0530

    ADDED: Employee daily wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,25 +1,31 @@
 package EmployeeWageComputation;
 
 
-    public class EmpWageBuilder {
-            public static void main(String args[]) {
+public class EmpWageBuilder {
+    public static void main(String args[]) {
 
-                // welcome message
-                System.out.println("Welcome to employee wage computation problem");
+        // welcome message
+        System.out.println("Welcome to employee wage computation problem");
 
-                // constants
-                final int IS_FULL_TIME = 1;
+        // constants
+        final int IS_FULL_TIME = 1;
+        final int EMP_RATE_PER_HR = 20;
 
-                // variables
-                double empCheck;
+        // variables
+        double empCheck;
+        int empHrs = 0;
+        int empWage = 0; // initial wage
 
-                /*
-                 * random function to get random numbers
-                 */
-                empCheck = Math.floor(Math.random() * 10) % 2;
-                if (empCheck == IS_FULL_TIME)
-                    System.out.println("Employee is Present");
-                else
-                    System.out.println("Employee is Absent");
-            }
+        /*
+         * random function to get random numbers
+         */
+        empCheck = Math.floor(Math.random() * 10) % 2;
+        // checking how many hrs employee has worked
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("EmpWage = " + empWage);
     }
+}
