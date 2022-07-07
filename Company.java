289f294ce49d85450f37e7f5742717c7ded80395
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-07 13:42:43 +0530

    ADDED: Total Wage for Each Company

---
+++ Company.java
@@ -0,0 +1,16 @@
+package EmployeeWageComputation;
+
+public class Company {
+    public final String companyName;
+    public final int empRatePerHr;
+    public final int numberOfWorkingDays;
+    public final int maxWorkingHrsPerMonth;
+
+    public Company(String companyName, int empRatePerHr, int numberOfWorkingDays, int maxWorkingHrsPerMonth) {
+        super();
+        this.companyName = companyName;
+        this.empRatePerHr = empRatePerHr;
+        this.numberOfWorkingDays = numberOfWorkingDays;
+        this.maxWorkingHrsPerMonth = maxWorkingHrsPerMonth;
+    }
+}
