af3801d2d39882167282614f840f94a3550e39a8
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-07 13:23:03 +0530

    START:Employee Wage Program

---
+++ EmpWageBuilder.java
@@ -0,0 +1,11 @@
+package EmployeeWageComputation;
+
+
+    public class EmpWageBuilder {
+
+        public static void main(String args[]) {
+
+            // welcome message
+            System.out.println("Welcome to employee wage computation problem");
+        }
+    }
