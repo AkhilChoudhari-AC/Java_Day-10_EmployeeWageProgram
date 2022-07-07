092e63e74e6f3735bb1fadaee0a1c1cc7808021c
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-07 13:34:18 +0530

    ADDED: Wages for the Month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,10 +3,11 @@ package EmployeeWageComputation;
 
 public class EmpWageBuilder {
 
-    // constants
+    //Constants
     final static int IS_FULL_TIME = 1;
     final static int EMP_RATE_PER_HR = 20;
     final static int IS_PART_TIME = 2;
+    final static int TOTAL_WORKING_DAYS = 20;
 
     public static void main(String args[]) {
         // welcome message
@@ -15,24 +16,31 @@ public class EmpWageBuilder {
         // variables
         double empCheck;
         int empHrs = 0;
-        int empWage = 0;
+        int totalEmpWage = 0;
+        int totalWorkingHrs = 0;
 
-        // computing employee daily working hrs by random method
-        empCheck = Math.floor(Math.random() * 10) % 3;
+        // calculating wages for total or maximum number of working days
+        for (int days = 0; days < TOTAL_WORKING_DAYS; days++) {
 
-        switch ((int) empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
+            empCheck = Math.floor(Math.random() * 10) % 3;
 
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
+            // checking each day working hrs
+            switch ((int) empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
 
-            default:
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+            }
+            System.out.println(empHrs);
+            totalWorkingHrs += empHrs; // adding each day working hour to get total employee working hrs
         }
-        empWage = empHrs * EMP_RATE_PER_HR;
-        System.out.println("EmpWage for a day " + empWage);
+        totalEmpWage = totalWorkingHrs * EMP_RATE_PER_HR;
+        System.out.println("total empWage for " + TOTAL_WORKING_DAYS + " is " + totalEmpWage);
     }
 }
